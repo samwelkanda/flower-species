aa80a4c0f1a51d8e6ff022b3a14e546eee1efaf9
Sam Kiptoo <samwelkanda@gmail.com>
2020-10-08 17:56:49 +0300

    Add prediction app and model

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # flower-species
+
 A simple machine learning application with Django REST framework, which predicts the species of a sample flower based on measurements of its features i.e. the sepal and petal dimensions – length and width.
+
+## Prerequisites
+
+This project ulilizes the following:
+
+* Python 3.7.4
+* Node 12.16.2
+* Postgres 12.3
+* Docker 19.03.12
+* docker-compose 1.26.2
+
+You need to have the above installed on your system.
+
+You also need a REST Api client to test the API.
+
+
+
--- backend/django_app/mainapp/settings.py
+++ backend/django_app/mainapp/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'prediction',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'mainapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'predictiondb',
+        'USER': 'postgres_user',
+        'PASSWORD': 'postgres_password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
